0b58eb934068b89c54406bfbd41362bd2627804d
Adeel Ahmad <adeel@hashicorp.com>
2021-06-30 16:16:55 +0100

    feat: added readme doc

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
-/* Copyright (C) Hashicorp, Inc - All Rights Reserved */
-/* Unauthorized copying of this file, via any medium is strictly prohibited */
-/* Proprietary and confidential */
-/* Written by Adeel Ahmad adeel@hashicorp.com, January 2021 */
+/**
+ * Usage:
+ *
+ * ```hcl
+ *
+ * module "mongodb" {
+ *   source                   = "git::https://github.com/devops-adeel/terraform-mongodb-cluster.git?ref=v0.1.0"
+ *   mongodb_atlas_project_id = var.project_id
+ *   cluster_name             = "application-dev"
+ * }
+ * ```
+ */
+
 
 locals {
   project_id = var.mongodb_atlas_project_id
